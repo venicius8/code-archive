a9601e265daff931faf76a8cfcecd339bfc4a5ff
Venícius <miguelsilva8v@gmail.com>
2025-10-22 23:54:41 -0300

    feat(freecodecamp): update boilerplate-mongomongoose

--- 2025/10/freecodecamp-backend-projects/boilerplate-mongomongoose/myApp.js
+++ 2025/10/freecodecamp-backend-projects/boilerplate-mongomongoose/myApp.js
@@ -59,7 +59,11 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, (err, data) => {
+    if (err) return done(err);
+
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
